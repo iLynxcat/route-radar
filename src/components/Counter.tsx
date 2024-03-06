53160f867f52e357fd1fb9cdfc63a0001485d77b
Lynx <ephemeral.lynx@protonmail.com>
2024-03-06 13:45:03 -0600

    Switch to SolidStart for inbuilt SSR

---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import "./Counter.css";
+import { createSignal } from "solid-js";
+
+export default function Counter() {
+	const [count, setCount] = createSignal(0);
+	return (
+		<button class="increment" onClick={() => setCount(count() + 1)}>
+			Clicks: {count()}
+		</button>
+	);
+}
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-/* @refresh reload */
-import App from "./App";
-import "./index.css";
-import { render } from "solid-js/web";
-
-const root = document.getElementById("root");
-
-render(() => <App />, root!);
